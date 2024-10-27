a8c9e1c496b0cb7407afe15bde4e6972c1dbe080
ndxbn <sitri.ndxbn@gmail.com>
2024-10-28 01:39:22 +0900

    feat: update something

--- bootstrapping/ubuntu.bash
+++ bootstrapping/ubuntu.bash
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -eu
 sudo apt update && sudo apt upgrade -y
-sudo apt install python3-venv python3-pip
+sudo apt install -y python3-venv python3-pip
 
 sudo locale-gen en_US.UTF-8
-localectl LANG=en_US.UTF-8
-lcoalectl LANGUAGE=en_US.UTF-8
+sudo localectl set-locale LANG=en_US.UTF-8
+sudo localectl set-locale LANGUAGE=en_US.UTF-8
 
 # under here, these should move to playbook.
-sudo apt install openssh-server
+sudo apt install -y openssh-server
 sudo systemctl start ssh
 sudo systemctl enable ssh
 
-sudo apt install stow zsh
+sudo apt install -y stow zsh
+
+# Ensure home directory structure
+mkdir -p ~/.local/{bin,tmp}
 
 # Install Homebrew and insatll some cli tools via brew
 git clone https://github.com/Homebrew/brew ~/.brew
